7214df07d194d42c1535adcbf50b1f71845a4d74
ghero56 <54166773+ghero56@users.noreply.github.com>
2021-01-30 22:47:41 -0600

    borron y cuenta nueva en main

---
+++ Code/definiciones.h
@@ -0,0 +1,11 @@
+#ifndef DEFINICIONES_H
+
+#define DEFINICIONES_H
+
+#include <iostream>
+
+using namespace std;
+
+/* ---------- DEFINICIONES --------- */
+
+#endif
---
+++ Code/main.cpp
@@ -0,0 +1,8 @@
+#include "funciones.cpp"
+
+/* ---------- Funcion main --------- */
+
+int main(int argc, char const *argv[])
+{
+  
+}
