0f1e670e5bb3fccf460a66d49f27435ce25ef0c8
ghero56 <54166773+ghero56@users.noreply.github.com>
2021-02-01 15:14:38 -0600

    Windows mode
    Solo compila grafos en windows

--- Grafos/lista.h
+++ Grafos/lista.h
@@ -1,40 +1,34 @@
 #include<iostream>
 using namespace std;
+
 class nodoa {
-   
-   public:
+  public:
     int ady;
     int peso;
     nodoa *siguiente;
 	//constructor
     nodoa(int v,int p, nodoa *sig = NULL)
     {
-       ady = v;
-       peso=p;
-       siguiente = sig;
+      ady = v;
+      peso=p;
+      siguiente = sig;
     }
-   
+
     class listaa;
 };
 
 typedef nodoa *pnodoa;
 
-class listaa {      
-   public:
+class listaa {
+  public:
     pnodoa primero;
     //constructor
 	listaa() { primero =  NULL; }// crea la lista vacia
 	//metodos
 	void Insertara(int v); // insert
-   // void Borrara(int v);// delete
+    // void Borrara(int v);// delete
     bool ListaaVacia();  //empty
-    void Mostrara();// recorrer	 
+    void Mostrara();// recorrer
     //pnodo Buscara( int v);// seek
-   // void BorrarListaa(); //clear
-    
+    // void BorrarListaa(); //clear
 };
-
-
-
-
-
