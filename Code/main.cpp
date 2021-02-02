740ab479929ec5955f825a9ee7d0d236dda5e916
ghero56 <54166773+ghero56@users.noreply.github.com>
2021-02-01 20:49:45 -0600

    VIVE!!!!!

--- Code/main.cpp
+++ Code/main.cpp
@@ -10,22 +10,21 @@
 int main(int argc, char const *argv[])
 {
   Grafo g;
-  array<string, 11> vertices={"K","J","I","H","G","F","E","D","C","B","A"};
-
-	array<string, 11> edges={"AE","AB","BC","BF","DH","EI","FG","FI","GJ","GK","HK"};
-
+  array<string, 5> vertices={"Ciencias","CU","Ing","UNAM","Metro"};
 
 	for( auto& v : vertices ){
 		g.add_vertice( Vertice( v ) );  // se añaden los vertices al grafo
 	}
 
-	for( auto& e : edges ){ // el auto& indica que el tipo a usar debe identificarlo el compilador (solo c++11)
-		g.add_estacion_dirigida( e.substr( 0, 1 ), e.substr( 1, 1 ) );
-		// .substr( a_partir_de_dónde, cuántos_queremos_leer )
-	}
-	g.print(); // imprimimos
+  g.add_estacion_dirigida( "Ciencias", "Ing" );
+  g.add_estacion_dirigida( "Ciencias", "Metro" );
+  g.add_estacion_dirigida( "CU", "Ciencias");
+  g.add_estacion_dirigida( "UNAM", "CU" );
+  g.add_estacion_dirigida( "UNAM", "Ciencias" );
 
-	g.bfs( "A" );
+  g.print(); // imprimimos
+
+	g.bfs( "UNAM" );
   //if(strcmp(argv[1],"admin") == 0) admin( );
   //else regular( );
   //system("clear");
