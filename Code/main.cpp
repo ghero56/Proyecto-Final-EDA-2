8487b70fb80bf908e53f1394862739b6ebdf564b
ghero56 <54166773+ghero56@users.noreply.github.com>
2021-02-02 23:52:45 -0600

    pocos avances

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,22 +1,21 @@
 #include "bdd.cpp"
 /**
-*
-*
-*
 * para compilar usar:
 *   g++ -Wall -std=c++11 -o main.out sqlite3.o main.cpp -ldl -pthread -fopenmp
 */
 /* ---------- Funcion main --------- */
 int main(int argc, char const *argv[])
 {
+#pragma omp parallel
+{
   #pragma omp master
   {
-  Grafo g;
-  BDD base;
-  int max = base.start( &g );
-
-  if(strcmp(argv[1],"admin") == 0) admin( base, &g , max);
-  else regular( base, &g , max);
+    Grafo g;
+    BDD base;
+    int max = base.start( &g );
+    if(strcmp(argv[1],"admin") == 0) admin( base, &g , max);
+    else regular( base, &g , max);
   }
+}
   return 0;
 }
