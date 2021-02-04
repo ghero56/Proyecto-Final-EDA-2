860a6388f831466273cfa083f4e5a1b0c62f3742
FernandoREX <74227680+FernandoREX@users.noreply.github.com>
2021-02-03 20:05:37 -0600

    Update main.cpp

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,19 +1,21 @@
 #include "bdd.cpp"
 /**
-* para compilar usar:
+* Comando para compilar:
 *   g++ -Wall -std=c++11 -o main.out sqlite3.o main.cpp -ldl -pthread -fopenmp
+*
+* @brief Funcion principal, encargada de hacer funcionar el codigo
 */
 /* ---------- Funcion main --------- */
 int main(int argc, char const *argv[])
 {
-#pragma omp parallel
+#pragma omp parallel //Abrimos regiones paralelas
 {
   #pragma omp master
   {
-    Grafo g;
-    BDD base;
+    Grafo g; //Crea un grafo de nombre "g" 
+    BDD base; //Creamos la base de datos esta tiene por nombre "base"
     int max = base.start( &g );
-    if(strcmp(argv[1],"admin") == 0) admin( base, &g , max);
+    if(strcmp(argv[1],"admin") == 0) admin( base, &g , max);    //Entramos a el programa como administradot o como refular
     else regular( base, &g , max);
   }
 }
